23fbee06dc1d2d98f3143a5d0a6db7930be2faf4
Janani Harihara Kumar <janani.signups@gmail.com>
2026-05-21 09:43:40 -0700

    Added base provider

--- setup.ps1
+++
@@ -1,9 +0,0 @@
-python -m venv venv
-# Windows
-.\\venv\\Scripts\\activate
-# Mac/Linux
-# source venv/bin/activate
-
-# install requirements and package
-pip install -r ./requirements/dev.txt
-pip install -e .
